351a909034b0876cb64069bd24edabb1e57ac473
markhamilton90 <msh387@gmail.com>
2016-02-08 20:53:41 -0600

    changed layout

--- app/app.js
+++ app/app.js
@@ -1,45 +1,63 @@
 
 angular.module('discoveri', [])
-	.factory('getArtist', function($http) {
-		return function(query) {
-			return $http.get('https://api.spotify.com/v1/search?q=' + query + '&type=artist');
+	.factory('spotifyData', function($http) {
+		return {
+			getArtist: function(query) {
+				return $http.get('https://api.spotify.com/v1/search?q=' + query + '&type=artist');
+			},
+			getTracks: function(id) {
+				return $http.get('https://api.spotify.com/v1/artists/' + id + '/top-tracks?country=US');
+			},
+			getRelatedArtists: function(id) {
+				return $http.get('https://api.spotify.com/v1/artists/' + id + '/related-artists');
+			},
+			getAlbums: function(ids) {
+				return $http.get('https://api.spotify.com/v1/albums?ids=' + ids);
+			}
 		}
 	})
-	.factory('getTracks', function($http) {
-		return function(id) {
-			return $http.get('https://api.spotify.com/v1/artists/' + id + '/top-tracks?country=US');
-		}
-	})
-	.factory('getRelatedArtists', function($http) {
-		return function(id) {
-			return $http.get('https://api.spotify.com/v1/artists/' + id + '/related-artists');
-		}
-	})
-	.factory('getAlbums', function($http) {
-		return function(ids) {
-			return $http.get('https://api.spotify.com/v1/albums?ids=' + ids);
-		}
-	})
-	.directive('backImage', function() {
-		return function(scope, element, attrs) {
-			attrs.$observe('backImage', function(value) {
-				element.css({
-					'background-image': 'url(' + value + ')'
-				});
-			});
-		}
-	})
-	.controller('MainCtrl', function($scope, $q, $http, getArtist, getTracks, 
-	getRelatedArtists, getAlbums) {
+	.controller('MainCtrl', function($scope, $q, spotifyData) {
 
 		$scope.tracksView = true;
 
+		
+
+
+		var callArtist = function(query) {
+			return spotifyData.getArtist(query).then(function(response) {
+				var artist = response.data.artists.items[0];
+				$scope.artist = artist.name;
+				$scope.artistId = artist.id;
+				$scope.images = artist.images[0].url;
+				$scope.followers = artist.followers.total;
+				return artist.id;
+			});
+		}
 
 
 
 
+		var callRelatedArtists = function(id) {
+			return spotifyData.getRelatedArtists(id).then(function(response) {
+				$scope.relatedArtists = response.data.artists;
+				console.log(response);
+				return id;
+			});
+		}
 
-		$scope.callAlbums = function(tracks) {
+
+
+
+		var callTracks = function(id) {
+			return spotifyData.getTracks(id).then(function(response) {
+				$scope.topTracks = response.data.tracks;
+				return response.data.tracks;
+			})
+		}
+
+
+
+		var callAlbums = function(tracks) {
 			var tracks = tracks;
 			var arrayOfAlbums = [];
 			for (var i = 0; i < tracks.length; i++) {
@@ -47,7 +65,7 @@ angular.module('discoveri', [])
 				else arrayOfAlbums.push(tracks[i].album.id);
 	 		}
 	 		var queryString = arrayOfAlbums.join(',');
-	 		getAlbums(queryString).then(function(response) {
+	 		spotifyData.getAlbums(queryString).then(function(response) {
 	 			$scope.albums = response.data.albums;
 	 		});
 		}
@@ -55,35 +73,22 @@ angular.module('discoveri', [])
 
 
 
-
-
-		$scope.callArtist = function(query) {
-			getArtist(query).then(function(response) {
-				$scope.artistId = response.data.artists.items[0].id;
-				$scope.images = response.data.artists.items[0].images[0].url;
-				$scope.artist = response.data.artists.items[0].name;
-				$scope.followers = response.data.artists.items[0].followers.total;
-				return response.data.artists.items[0].id;
-			})
-			.then(function(response) { 
-				// get related artists via artist ID
-				getRelatedArtists(response).then(function(response) {
-					$scope.relatedArtists = response.data.artists;
+		$scope.getArtistInfo = function(query) {
+			callArtist(query)
+			.then(function(response) {
+				callRelatedArtists(response);
+				callTracks(response).then(function(response) {
+					callAlbums(response);
 				});
-				// get top tracks via artist ID
-				var tracks = getTracks(response).then(function(response) {
-					$scope.topTracks = response.data.tracks;
-					return response.data.tracks;
-				});
-				return tracks;
-			})
-			.then(function(tracks) {
-				// get albums via top tracks
-				$scope.callAlbums(tracks);
 			});
 		}
 
-		$scope.callArtist('Rise Against');
+
+
+
+
+
+		$scope.getArtistInfo('Rise Against');
 
 		
 	});
